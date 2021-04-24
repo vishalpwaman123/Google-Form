62dc25154416dc53a3caa2371198289e8dfa0305
vishalpwaman <63911951+vishalpwaman@users.noreply.github.com>
2021-04-24 11:27:46 +0530

    Added:Sign In Page

--- googleforms-project/src/App.js
+++ googleforms-project/src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SignUp from "./components/SignUp/SignUp.jsx";
+import SignIn from "./components/SignIn/SignIn.jsx";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/" component={SignUp} />
+          <Route exact path="/" component={SignUp} />
+          <Route exact path="/SignIn" component={SignIn} />
         </Switch>
       </Router>
     </div>
---
+++ googleforms-project/src/components/SignIn/SignIn.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import "./SignIn.scss";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+export class SignIn extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="signIn_Container">
+        <div className="sub_Container">
+          <div className="inner_Container">
+            <div className="google_Header">
+              <span className="G">G</span>
+              <span className="o1">o</span>
+              <span className="o2">o</span>
+              <span className="g">g</span>
+              <span className="l">l</span>
+              <span className="e">e</span>
+            </div>
+            <div className="body">
+              <div className="signIn_Header">
+                <div className="signIn_Inner">Sign in</div>
+              </div>
+              <div className="sub_Header">
+                <div className="sub_Inner">Use your Google Account</div>
+              </div>
+              <div className="signIn_Body">
+                <div className="input_Field">
+                  <TextField
+                    className="Em_InputField"
+                    label="Email or phone"
+                    variant="outlined"
+                    //   value={}
+                    //   onChange={}
+                  />
+                </div>
+                <div className="forget_email">
+                  <Button color="primary" className="f_email">
+                    Forgot email?
+                  </Button>
+                </div>
+                <div className="suggestion_Text">
+                  Not your computer? Use Guest mode to sign in privately.
+                </div>
+                <div className="learnmore_Button">
+                  <Button color="primary" className="lm_Button">
+                    Learn more
+                  </Button>
+                </div>
+                <div className="bottons">
+                  <div className="create_Account">
+                    <Button color="primary" className="ca_Button">
+                      Create account
+                    </Button>
+                  </div>
+                  <div className="next_Button">
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      className="n_Button"
+                    >
+                      Next
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="button_Container"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
